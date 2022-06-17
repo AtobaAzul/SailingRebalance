e75d4850b9f0b6ec7e53097bf4ad4fcbe6556cd6
AtobaAzul <atoba.azul.radical@gmail.com>
2022-06-17 11:05:31 -0300

    Seasonal shoals rework, should function a lot better

--- init/postinit/prefabs/seasonal_shoals.lua
+++ init/postinit/prefabs/seasonal_shoals.lua
@@ -1,171 +1,101 @@
 if GetModConfigData("config_seasonalShoals") then
-    AddPrefabPostInit("oceanfish_shoalspawner", function(inst)
-        if not GLOBAL.TheWorld.ismastersim then
-            return
-        end
+    local env = env
+    GLOBAL.setfenv(1, GLOBAL)
+    -----------------------------------------------------------------
+    local function OnSeasonTick(inst)
         if inst.components.childspawner ~= nil then
-            if GLOBAL.TheWorld.state.isautumn == true then
+            if TheWorld.state.isautumn then
                 inst.components.childspawner:SetRareChild("oceanfish_small_6", 0.2)
-            elseif GLOBAL.TheWorld.state.iswinter == true then
+                inst.components.childspawner.childname = "oceanfish_medium_2"
+            elseif TheWorld.state.iswinter == true then
                 inst.components.childspawner:SetRareChild("oceanfish_medium_8", 0.2)
                 inst.components.childspawner.childname = "oceanfish_medium_4"
-            elseif GLOBAL.TheWorld.state.isspring == true then
+            elseif TheWorld.state.isspring == true then
                 inst.components.childspawner:SetRareChild("oceanfish_small_7", 0.2)
                 inst.components.childspawner.childname = "oceanfish_medium_5"
-            elseif GLOBAL.TheWorld.state.issummer == true then
+            elseif TheWorld.state.issummer == true then
                 inst.components.childspawner:SetRareChild("oceanfish_small_8", 0.2)
                 inst.components.childspawner.childname = "oceanfish_medium_3"
             end
+        else
         end
+    end
+    
+    env.AddPrefabPostInit("oceanfish_shoalspawner", function(inst)
+        --if not TheWorld.ismastersim then
+        --    print("NOT MASTERSIM")
+        --    return
+        --end
+        inst:DoTaskInTime(0, OnSeasonTick)
+        inst:WatchWorldState("phase", OnSeasonTick)
     end)
-
-    AddPrefabPostInit("oceanfish_medium_2", function(inst)
-        local function OnLoad(inst, data)
-            if data ~= nil and data.herdprefab ~= nil then
-                inst.components.herdmember.herdprefab = data.herdprefab
-            end
-
-            if data ~= nil and data.heavy then
-                inst.heavy = data.heavy
-            end
-
-            if not GLOBAL.TheWorld.state.isautumn then
-                if inst.sg ~= nil then
-                    inst.sg:GoToState("leave")
-                else
-                    inst:Remove()
+    
+    local autumn_fishes = {"oceanfish_small_6", "oceanfish_medium_2"}
+    local winter_fishes = {"oceanfish_medium_4", "oceanfish_medium_8"}
+    local spring_fishes = {"oceanfish_small_7", "oceanfish_medium_5"}
+    local summer_fishes = {"oceanfish_small_8", "oceanfish_medium_3"}
+    
+    for k,v in ipairs(autumn_fishes) do
+        env.AddPrefabPostInit(v, function(inst)
+            inst:WatchWorldState("phase", function(inst)
+                if (inst.components.homeseeker ~= nil and inst.components.homeseeker.home.prefab == "oceanfish_shoalspawner") and not TheWorld.state.isautumn then
+                    if inst.sg ~= nil then
+                        if not  inst.sg:HasStateTag("busy") then
+                            inst:DoTaskInTime(math.random(1,5), inst.sg:GoToState("leave"))
+                        end
+                    else
+                        inst:Remove()
+                    end
                 end
-            end
-        end
-        inst.OnLoad = OnLoad
-    end)
-
-    AddPrefabPostInit("oceanfish_small_6", function(inst)
-        local function OnLoad(inst, data)
-            if data ~= nil and data.herdprefab ~= nil then
-                inst.components.herdmember.herdprefab = data.herdprefab
-            end
-
-            if data ~= nil and data.heavy then
-                inst.heavy = data.heavy
-            end
-
-            if not GLOBAL.TheWorld.state.isautumn then
-                if inst.sg ~= nil then
-                    inst.sg:GoToState("leave")
-                else
-                    inst:Remove()
+            end)
+        end)
+    end
+    
+    for k,v in ipairs(winter_fishes) do
+        env.AddPrefabPostInit(v, function(inst)
+            inst:WatchWorldState("phase", function(inst)
+                if (inst.components.homeseeker ~= nil and inst.components.homeseeker.home.prefab == "oceanfish_shoalspawner") and not TheWorld.state.iswinter then
+                    if inst.sg ~= nil then
+                        if not  inst.sg:HasStateTag("busy") then
+                            inst:DoTaskInTime(math.random(1,5), inst.sg:GoToState("leave"))
+                        end
+                    else
+                        inst:Remove()
+                    end
                 end
-            end
-        end
-        inst.OnLoad = OnLoad
-    end)
-
-    AddPrefabPostInit("oceanfish_medium_4", function(inst)
-        local function OnLoad(inst, data)
-            if data ~= nil and data.herdprefab ~= nil then
-                inst.components.herdmember.herdprefab = data.herdprefab
-            end
-
-            if data ~= nil and data.heavy then
-                inst.heavy = data.heavy
-            end
-
-            if not GLOBAL.TheWorld.state.iswinter then
-                if inst.sg ~= nil then
-                    inst.sg:GoToState("leave")
-                else
-                    inst:Remove()
+            end)
+        end)
+    end
+    
+    for k,v in ipairs(spring_fishes) do
+        env.AddPrefabPostInit(v, function(inst)
+            inst:WatchWorldState("phase", function(inst)
+               if (inst.components.homeseeker ~= nil and inst.components.homeseeker.home.prefab == "oceanfish_shoalspawner") and not TheWorld.state.isspring then
+                    if inst.sg ~= nil then
+                        if not  inst.sg:HasStateTag("busy") then
+                            inst:DoTaskInTime(math.random(1,5), inst.sg:GoToState("leave"))
+                        end
+                    else
+                        inst:Remove()
+                    end
                 end
-            end
-        end
-        inst.OnLoad = OnLoad
-    end)
-
-    AddPrefabPostInit("oceanfish_medium_8", function(inst)
-        local function OnLoad(inst, data)
-            if data ~= nil and data.herdprefab ~= nil then
-                inst.components.herdmember.herdprefab = data.herdprefab
-            end
-
-            if data ~= nil and data.heavy then
-                inst.heavy = data.heavy
-            end
-
-            if not GLOBAL.TheWorld.state.iswinter then
-                if inst.sg ~= nil then
-                    inst.sg:GoToState("leave")
-                else
-                    inst:Remove()
+            end)
+        end)
+    end
+    
+    for k,v in ipairs(summer_fishes) do
+        env.AddPrefabPostInit(v, function(inst)
+            inst:WatchWorldState("phase", function(inst)
+               if (inst.components.homeseeker ~= nil and inst.components.homeseeker.home.prefab == "oceanfish_shoalspawner") and not TheWorld.state.issummer then
+                    if inst.sg ~= nil then
+                        if not  inst.sg:HasStateTag("busy") then
+                            inst:DoTaskInTime(math.random(1,5), inst.sg:GoToState("leave"))
+                        end
+                    else
+                        inst:Remove()
+                    end
                 end
-            end
-        end
-        inst.OnLoad = OnLoad
-    end)
-
-    AddPrefabPostInit("oceanfish_medium_5", function(inst)
-        local function OnLoad(inst, data)
-            if data ~= nil and data.herdprefab ~= nil then
-                inst.components.herdmember.herdprefab = data.herdprefab
-            end
-
-            if data ~= nil and data.heavy then
-                inst.heavy = data.heavy
-            end
-
-            if not GLOBAL.TheWorld.state.isspring then
-                if inst.sg ~= nil then
-                    inst.sg:GoToState("leave")
-                else
-                    inst:Remove()
-                end
-            end
-        end
-        inst.OnLoad = OnLoad
-    end)
-
-    AddPrefabPostInit("oceanfish_small_7", function(inst)
-        local function OnLoad(inst, data)
-            if data ~= nil and data.herdprefab ~= nil then
-                inst.components.herdmember.herdprefab = data.herdprefab
-            end
-
-            if data ~= nil and data.heavy then
-                inst.heavy = data.heavy
-            end
-
-            if not GLOBAL.TheWorld.state.isspring then
-                if inst.sg ~= nil then
-                    inst.sg:GoToState("leave")
-                else
-                    inst:Remove()
-                end
-            end
-        end
-        inst.OnLoad = OnLoad
-    end)
-
-    AddPrefabPostInit("oceanfish_small_8", function(inst)
-        if not GLOBAL.TheWorld.ismastersim then
-            return
-        end
-        local function OnLoad(inst, data)
-            if data ~= nil and data.herdprefab ~= nil then
-                inst.components.herdmember.herdprefab = data.herdprefab
-            end
-
-            if data ~= nil and data.heavy then
-                inst.heavy = data.heavy
-            end
-
-            if not GLOBAL.TheWorld.state.issummer then
-                if inst.sg ~= nil then
-                    inst.sg:GoToState("leave")
-                else
-                    inst:Remove()
-                end
-            end
-        end
-        inst.OnLoad = OnLoad
-    end)
+            end)
+        end)
+    end
 end
